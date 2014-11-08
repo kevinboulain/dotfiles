1d37b305deed498fc90e070292a06e97159c217d
Kevin "ether" Boulain <kevinboulain@gmail.com>
2014-11-08 23:54:05 +0100

    a bit of restructuration

--- python/log.py
+++ python/log.py
@@ -1,10 +1,11 @@
-import types
 import functools
-import logging
 
+import logging
 logger = logging.getLogger(__name__)
 
-def set_root(level):
+stringify = str
+
+def set_root(level='warning'):
     """
     Set the minimum level for root logger
 
@@ -20,96 +21,95 @@ def set_root(level):
     # set the minimum level for the root logger
     root_logger.setLevel(getattr(logging, level.upper()))
 
-def stringify(argument):
-    """
-    Return a 'stringified' representation of the passed argument
 
-    :param str argument: an object to be stingified
-    :rtype: str
+def log_function_call(level, function, *args, **kwargs):
     """
-    # do not call repr() on classes
-    # this types can be stringified without any further processing
-    if isinstance(argument, (
-            types.NoneType, types.BooleanType,
-            types.IntType, types.LongType, types.FloatType, types.ComplexType,
-            types.StringTypes,
-            #types.TupleType, types.ListType, types.DictType,
-            )):
-        return str(argument)
-    # function
-    elif isinstance(argument, (types.FunctionType, types.LambdaType,)):
-        return str(argument.func_name)
-    # list kind tuple
-    elif isinstance(argument, (types.TupleType, types.ListType,)):
-        return str('[%s]' % ', '.join([
-            stringify(arg) for arg in argument
-        ]))
-    # dict type
-    elif isinstance(argument, (types.DictType,)):
-        return str('{%s}' % ', '.join([
-            '%s: %s' % (stringify(key), stringify(value))
-            for key, value in argument.iteritems() # iteritems: python2
-        ]))
-    # handle everything else, treated as an object
-    else:
-        # we have a new-style class
-        if isinstance(argument, type):
-            return str('%s new-style class' % argument.__name__)
-        # we have an old style class
-        if not hasattr(argument, '__class__'):
-            return str('%s old-style class' % argument.__name__)
-        # we have an instance
-        if isinstance(argument, argument.__class__):
-            return str('%s instance' % argument.__class__.__name__)
-    # should never get here
-    assert 0, "can't stringify %s" % str(argument)
+    A closure that handle the formatting and the logging
+
+    :param str level: a specific log level for this log call
+    :param function function: the function that will be called
+    :param list *args: the unnamed function parameters
+    :param list **kwargs: the named function parameters
+    :rtype: dict
+    """
+    # handle the log level tests here, before any processing
+    log_level = getattr(logging, level.upper())
+
+    # avoid useless processing of parameters
+    # by only treating messages that will be processed by the logger
+    if not logger.isEnabledFor(log_level): return lambda **kwargs: None
+
+    # the log function that will be used
+    log_function = getattr(logger, level.lower())
+
+    # the arguments of the function call
+    arguments = ', '.join([stringify(arg) for arg in args])
+    keywords_arguments = ', '.join([
+        '%s=%s' % (key, stringify(value))
+        for key, value in kwargs.iteritems() # iteritems: python2
+    ])
+
+    # the call informations
+    call_informations = {
+        'call': call.func_name, # the call function name
+        'module': function.__module__, # the module name
+        'function': function.func_name, # the function called
+        'parameters': ', '.join( # the call parameters
+            [arguments] if arguments else [] +
+            [keywords_arguments] if keywords_arguments else []),
+    }
+
+    def log_formatted(**contents):
+        """
+        The formatting and logging function
+
+        :param dict **contents: the format contents
+        """
+        log_message = (
+            '{call}: {module}.{function}({parameters}) {what} {value}'
+        ).format(**contents)
+        return log_function(log_message.rstrip())
+    return lambda what='', value='': log_formatted(
+        what=stringify(what),
+        value=stringify(value),
+        **call_informations
+    )
 
 def call(function=None, level='debug'):
     '''
     Decorator to debug functions calls.
-    May decrease performance!
 
+    :param function function: the decorated function
     :param str level: a specific log level for this log call
+    :rtype: function
     '''
     # if there is no function, it has been called with arguments
     # returns the decorator with the optionals arguments for the next call
-    if function is None:
-        return functools.partial(call, level=level)
+    if function is None: return functools.partial(call, level=level)
+
     # frunctools.wraps allow to not loose the function informations
     @functools.wraps(function)
-    def log_then_call(*args, **kwargs):
-        # avoid useless processing of parameters
-        # by only treating messages that will be processed by the logger
-        if logger.isEnabledFor(getattr(logger, level)):
-            # arguments
-            arguments = ', '.join([
-                stringify(arg) for arg in args
-            ])
-            # keyword arguments
-            keywords_arguments = ', '.join([
-                '%s=%s' % (key, stringify(value))
-                for key, value in kwargs.items()
-            ])
-            if arguments and keywords_arguments:
-                parameters = '%s, %s' % (arguments, keywords_arguments)
-            elif arguments or keywords_arguments:
-                parameters = '%s' % (
-                    arguments if arguments else keywords_arguments
-                    )
-            else: parameters = ''
-            if level not in call.available_levels:
-                raise ValueError(
-                    "Unknown log level '%s', available ones: %s" % (
-                        level, ', '.join(call.available_levels)
-                    )
-                )
-            getattr(logger, level)(
-                '%s: %s.%s(%s)' % (
-                    call.func_name, function.__module__,
-                    function.__name__, parameters
-                )
-            )
-        return function(*args, **kwargs)
-    return log_then_call
+    def log_call(*args, **kwargs):
+        """
+        The inner function of the call decorator
 
-call.available_levels = ('critical', 'error', 'warning', 'info', 'debug')
+        :param list *args: the unnamed function parameters
+        :param list **kwargs: the named function parameters
+        """
+        # create a closure that will store every call related variables
+        log_function = log_function_call(level, function, *args, **kwargs)
+
+        # log that the function has been called
+        log_function()
+
+        # do the call
+        try: value = function(*args, **kwargs)
+        except Exception as exception:
+            # log that the function has raised an exception
+            log_function(what='raise', value=exception)
+            raise exception
+
+        # log that the function has returned
+        log_function(what='return', value=value)
+        return value
+    return log_call

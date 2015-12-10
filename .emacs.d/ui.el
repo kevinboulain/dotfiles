3cfa5118ac5c10af7075bb978656cb327ec77577
Kevin Boulain <kevin.boulain@securactive.net>
2015-12-10 12:45:19 +0100

    emacs: fix linum display and line counting

--- .emacs.d/ui.el
+++ .emacs.d/ui.el
@@ -8,7 +8,25 @@
 
   ; show lines numbers, see http://www.emacswiki.org/LineNumbers
   (when (require 'linum nil t)
-    (setq linum-format "%d ") ; add a blank space after the line number
+    (add-hook 'linum-before-numbering-hook
+      (lambda () ; make line counting more efficient
+        (setq-local linum-format-fmt
+          (let ; fix the numbering issue that move the buffer to the right every new digit
+            ((w (length (number-to-string (count-lines (point-min) (point-max))))))
+            (concat "%" (number-to-string w) "d")
+          )
+        )
+      )
+    )
+    (setq linum-format
+      (lambda (line)
+        (concat
+          (propertize (format linum-format-fmt line) 'face 'linum)
+          (propertize " " 'face 'linum)
+        )
+      )
+    )
+    ; (setq linum-format "%d ") ; add a blank space after the line number
     (global-linum-mode 1)
   )
 )

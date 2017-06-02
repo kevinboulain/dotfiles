7b493c5eeaba5325f718666b928f00553abbb849
Kevin Boulain <kevinboulain@gmail.com>
2017-06-02 21:40:28 +0200

    bash: history timestamping

--- .bash/history.bash
+++ .bash/history.bash
@@ -2,6 +2,9 @@ HISTCONTROL=ignoredups # ignore duplicates in history
 HISTCONTROL+=:erasedups # remove duplicates from history
 HISTCONTROL+=:ignorespace # don't store lines beginning with spaces in history
 
+# date format for 'history'
+HISTTIMEFORMAT='%F %T '
+
 # append to history, do not overwrite it
 shopt -s histappend
 

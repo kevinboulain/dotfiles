0cd1fc7ba25383bcc9bbff36488b47389322f504
Kevin Boulain <kevinboulain@gmail.com>
2017-06-02 22:49:47 +0200

    bash: path cleanup

--- .bash/path.bash
+++ .bash/path.bash
@@ -1,1 +1,13 @@
-export PATH='/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin'
+IFS=: read -r -a paths <<< "$PATH"
+declare -A unique_paths
+PATH=
+# standard locations comes after any user-defined $PATH
+for path in /{,usr/{,local/}}{bin,sbin} "${paths[@]}"; do
+    # if path exists and if it isn't already in path, prepend it
+    if [ -d "$path" ] && [ -z "${unique_paths[$path]+_}" ]; then
+        unique_paths[$path]=_
+        PATH=$path${PATH:+:$PATH}
+    fi
+done
+unset unique_paths paths path
+export PATH

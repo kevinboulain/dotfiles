ffebf0d344d7b90b6a52dc70e6ef40c531982dbc
Kevin Boulain <kevinboulain@gmail.com>
2017-07-15 11:32:27 +0200

    bash: put the path ordering logic into a function

--- .bash/path.bash
+++ .bash/path.bash
@@ -1,13 +1,18 @@
-IFS=: read -r -a paths <<< "$PATH"
-declare -A unique_paths
-PATH=
-# standard locations comes after any user-defined $PATH
-for path in /{,usr/{,local/}}{bin,sbin} "${paths[@]}"; do
+order_path() {
+  local -a path
+  IFS=: read -r -a paths <<< "$PATH"
+  local -A unique_paths
+  PATH=
+  local path
+  # standard locations comes after any user-defined $PATH
+  for path in /{,usr/{,local/}}{bin,sbin} "${paths[@]}"; do
     # if path exists and if it isn't already in path, prepend it
     if [ -d "$path" ] && [ -z "${unique_paths[$path]+_}" ]; then
-        unique_paths[$path]=_
-        PATH=$path${PATH+:$PATH}
+      unique_paths[$path]=_
+      PATH=$path${PATH+:$PATH}
     fi
-done
-unset unique_paths paths path
-export PATH
+  done
+  export PATH
+}
+
+order_path

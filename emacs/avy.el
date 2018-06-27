db84a45f5b27a65118b05e79cbe570b9154fd038
Kevin Boulain <kevinboulain@gmail.com>
2018-06-27 23:09:52 +0200

    emacs: avy-goto-char-2

--- emacs/avy.el
+++ emacs/avy.el
@@ -3,4 +3,4 @@
   :quelpa ((avy :fetcher github :repo "abo-abo/avy"))
   :config
   ;; I think I never used open-line, so override default binding
-  (global-set-key (kbd "C-o") 'avy-goto-char))
+  (global-set-key (kbd "C-o") 'avy-goto-char-2))

7fae502c3df33d616d0014ae68b04bf57783ea49
Kevin Boulain <kevinboulain@gmail.com>
2018-08-04 15:18:54 +0200

    emacs: htmlize fixes

--- emacs/htmlize.el
+++ emacs/htmlize.el
@@ -7,20 +7,20 @@
                              ;; flycheck handling (use special faces that are not nicely rendered)
                              (when (bound-and-true-p flycheck-mode) (flycheck-mode))
                              ;; disable current line highlighting (both a function and a variable)
-                             (when (bound-and-true-p global-hl-line-mode) (global-hl-line-mode -1))
+                             (when (bound-and-true-p global-hl-line-mode) (global-hl-line-unhighlight))
                              ;; disable line numbering (the format adds a an empty column)
-                             (when (bound-and-true-p nlinum-mode) (global-nlinum-mode 0))
+                             (when (bound-and-true-p nlinum-mode) (nlinum-mode))
+                             ;; TODO: lsp stuff
                              ;; (sit-for 3) ; allow to see the changes made to the buffer
                              ))
          (htmlize-before . (lambda ()
                              ;; use the Iosevka font if available (ligatures may be nice for displayed code)
-                             (setq htmlize-head-tags
-                                   ;; requires fontconfig, works on Linux and macOS
-                                   ;; use %{=unparse} format to see all options
-                                   (let* ((fc-match "fc-match -f '%{file}' 'Iosevka:style=Regular'")
-                                          (path (shell-command-to-string fc-match)))
-                                     (when (length path)
-                                       (format "    <style type=\"text/css\">
+                             ;; requires fontconfig, works on Linux and macOS
+                             ;; use %{=unparse} format to see all options
+                             (let* ((fc-match "fc-match -f '%{file}' 'Iosevka:style=Regular'")
+                                    (path (shell-command-to-string fc-match)))
+                               (when (s-suffix? ".ttf" path :ignore-case) ; Firefox doesn't like .ttc
+                                 (setq htmlize-head-tags (format "    <style type=\"text/css\">
       @font-face {
         font-family: Iosevka;
         src: url(data:font/ttf;base64,%s) format('truetype');
@@ -31,4 +31,4 @@
       }
     </style>
 "
-                                               (base64-encode-string (with-temp-buffer (insert-file-contents path) (buffer-string)) t)))))))))
+                                                                 (base64-encode-string (with-temp-buffer (insert-file-contents path) (buffer-string)) t)))))))))

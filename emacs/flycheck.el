ccd984f8f8284ac7f719bb34eaf07dd4e1adeab9
Kevin Boulain <kevinboulain@gmail.com>
2018-08-04 15:18:31 +0200

    emacs: hide flycheck files

--- emacs/flycheck.el
+++ emacs/flycheck.el
@@ -3,4 +3,5 @@
   :quelpa ((flycheck :fetcher github :repo "flycheck/flycheck"))
   :hook (prog-mode . flycheck-mode)
   :config
-  (setq flycheck-checker-error-threshold nil))
+  (setq flycheck-checker-error-threshold nil)
+  (setq flycheck-temp-prefix ".flycheck"))

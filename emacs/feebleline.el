8fb33227a658f4fc10a7359f353cb6f90d3faf24
Kevin Boulain <kevinboulain@gmail.com>
2018-09-27 17:53:55 +0000

    emacs: feebline mode-line setup

--- emacs/feebleline.el
+++ emacs/feebleline.el
@@ -2,12 +2,37 @@
 (use-package feebleline
   :ensure t
   :quelpa ((feebleline :fetcher github :repo "tautologyclub/feebleline"))
-  :hook
-  ;; or won't be able to show notifications
-  ;; should find a way to toggle it based on circe (de)activation
-  (circe-mode . (lambda () (feebleline-mode 0)))
   :config
-  (setq feebleline-show-git-branch t)
-  (setq feebleline-show-directory t)
-  (setq feebleline-show-linenum nil)
+  ;; mainly from feebleline.el, but more minimalistic
+  (setq feebleline-mode-line-text
+        '(("%s" ((if (buffer-file-name)
+                     (replace-regexp-in-string
+                      feebleline--home-dir "~"
+                      (file-name-directory (buffer-file-name)))
+                   ""))
+           (face feebleline-dir-face))
+          ("%s" ((if (buffer-file-name)
+                     (file-name-nondirectory (buffer-file-name))
+                   (buffer-name)))
+           (face feebleline-bufname-face))
+          ("%s" ((if (and (buffer-file-name) (buffer-modified-p))
+                     "*"
+                   ""))
+           (face feebleline-asterisk-face))
+          ("%s" ((if (fboundp 'magit-get-current-branch) ; set by magit
+                     (let ((branch (magit-get-current-branch)))
+                       (if (> (length branch) 0) (concat ":" branch) ""))
+                   ""))
+           (face feebleline-git-branch-face))
+          ("%s" ; for circe, display the list of active buffers
+           ((if (boundp 'tracking-buffers) ; set by circe
+                (concat " " (string-join
+                             (mapcar (lambda (buffer)
+                                       (let ((propertized-buffer (propertize buffer 'face 'feebleline-previous-buffer-face))) ; change the face
+                                         (if (eq (get-text-property 0 'face buffer) 'circe-highlight-nick-face) ; when highlighted
+                                             (concat propertized-buffer (propertize "*" 'face 'feebleline-asterisk-face)) ; append a feebleline asterisk
+                                           propertized-buffer)))
+                                     tracking-buffers) ; for each buffer
+                             " "))
+              "")))))
   (feebleline-mode 1))

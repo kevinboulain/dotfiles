628ad9cdfbaa9764532fd8b5ead6e2fc7f771d92
Kevin Boulain <kevinboulain@gmail.com>
2018-09-29 14:34:46 +0200

    emacs: ace-window

--- emacs/avy.el
+++ emacs/avy.el
@@ -2,4 +2,12 @@
   :straight (:host github :repo "abo-abo/avy")
   :config
   ;; I think I never used open-line, so override default binding
-  (global-set-key (kbd "C-o") 'avy-goto-char-2))
+  (global-set-key (kbd "C-o") 'avy-goto-word-or-subword-1))
+
+;; despite the name, it's avy-based
+(use-package ace-window
+  :straight (:host github :repo "abo-abo/ace-window")
+  :config
+  (setq aw-background nil) ; don't remove colors
+  ;; override facemenu default binding
+  (global-set-key (kbd "M-o") 'ace-window))

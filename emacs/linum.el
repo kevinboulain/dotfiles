01553900cd1aa928340b2add273c7142f6f09250
Kevin Boulain <kevinboulain@gmail.com>
2018-09-30 17:42:22 +0200

    emacs: customize mode line for tao theme

--- emacs/linum.el
+++ emacs/linum.el
@@ -2,7 +2,7 @@
   :straight (:host github :repo "emacsmirror/nlinum")
   :config
   ;; highlight the margin with the same line highlighting
-  (set-face-attribute 'nlinum-current-line nil ; replace with :custom-face?
+  (set-face-attribute 'nlinum-current-line nil
                       :foreground (face-attribute 'linum :foreground nil t)
                       :background (face-attribute 'hl-line :background nil t))
   (setq nlinum-format "%d ")
--- emacs/tao.el
+++ emacs/tao.el
@@ -1,2 +1,8 @@
 (use-package tao-yin-theme
-  :straight (tao-theme :host github :repo "11111000000/tao-theme-emacs"))
+  :straight (tao-theme :host github :repo "11111000000/tao-theme-emacs")
+  :config
+  ;; override the mode-line{,-inactive} faces to match vertical-border
+  (dolist (face '(mode-line mode-line-inactive))
+    (set-face-attribute face nil
+                        :foreground (face-attribute 'vertical-border :foreground nil t)
+                        :background (face-attribute 'vertical-border :background nil t))))

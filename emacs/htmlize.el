4ab2544b47a131e07932bf6b28ca10c24b31fb67
Kevin Boulain <kevinboulain@gmail.com>
2018-10-07 01:43:51 +0200

    emacs: simplify htmlize configuration

--- emacs/htmlize.el
+++ emacs/htmlize.el
@@ -36,6 +36,27 @@
   :straight (:host github :repo "hniksic/emacs-htmlize")
   :init
   (add-to-list 'command-switch-alist '("--htmlize" . ether--htmlize-file))
+  :config
+  ;; use the Iosevka font if available (ligatures may be nice for displayed code)
+  ;; requires fontconfig, works on Linux and macOS
+  ;; use %{=unparse} format to see all options
+  ;; regular Iosevka instead of Iosevka Term seems to produce some irregularities
+  ;; for example let-alist and use-package seems to be slightly larger
+  (let* ((fc-match "fc-match -f '%{file}' 'Iosevka Term:style=Regular'")
+         (path (shell-command-to-string fc-match)))
+    (when (s-suffix? ".ttf" path :ignore-case) ; Firefox doesn't like .ttc
+      (setq htmlize-head-tags (format "    <style type=\"text/css\">
+      @font-face {
+        font-family: Iosevka;
+        src: url(data:font/ttf;base64,%s) format('truetype');
+      }
+      pre {
+        font-family: Iosevka;
+        font-size: 10pt;
+      }
+    </style>
+"
+                                      (base64-encode-string (with-temp-buffer (insert-file-contents path) (buffer-string)) t)))))
   :hook ((htmlize-before . (lambda ()
                              ;; disable some modes that may influence rendering
                              ;; the original buffer is protected, no need to restore
@@ -48,28 +69,5 @@
                              (when (bound-and-true-p nlinum-mode) (nlinum-mode 0))
                              ;; TODO: lsp stuff
                              ;; (sit-for 3) ; allow to see the changes made to the buffer
-                             ))
-         (htmlize-before . (lambda ()
-                             (require 's)
-                             ;; use the Iosevka font if available (ligatures may be nice for displayed code)
-                             ;; requires fontconfig, works on Linux and macOS
-                             ;; use %{=unparse} format to see all options
-                             ;; regular Iosevka instead of Iosevka Term seems to produce some irregularities
-                             ;; for example let-alist and use-package seems to be slightly larger
-                             (let* ((fc-match "fc-match -f '%{file}' 'Iosevka Term:style=Regular'")
-                                    (path (shell-command-to-string fc-match)))
-                               (when (s-suffix? ".ttf" path :ignore-case) ; Firefox doesn't like .ttc
-                                 (setq htmlize-head-tags (format "    <style type=\"text/css\">
-      @font-face {
-        font-family: Iosevka;
-        src: url(data:font/ttf;base64,%s) format('truetype');
-      }
-      pre {
-        font-family: Iosevka;
-        font-size: 10pt;
-      }
-    </style>
-"
-                                                                 (base64-encode-string (with-temp-buffer (insert-file-contents path) (buffer-string)) t)))))))))
-
+                             ))))
 ;;; htmlize.el ends here

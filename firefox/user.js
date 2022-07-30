e02e438c5b97366d4f8da1082b0bf05175c413b2
Kevin Boulain <kevinboulain@gmail.com>
2022-07-30 20:50:03 +0200

    nixos: wayland screensharing

--- firefox/user.js
+++ firefox/user.js
@@ -2,3 +2,6 @@
 // behave like other applications.
 // https://bugzilla.mozilla.org/show_bug.cgi?id=1752862
 user_pref("apz.gtk.kinetic_scroll.delta_mode", 2);
+
+// https://shivering-isles.com/til/2021/07/firefox-webrtc-popup
+user_pref("privacy.webrtc.legacyGlobalIndicator", false);
--- nixos/common/wayland.nix
+++ nixos/common/wayland.nix
@@ -16,6 +16,16 @@
 
   environment.systemPackages = with pkgs; [
     firefox-wayland
+    grim
+    slurp
     vlc
   ];
+
+  # For screensharing (via WebRTC).
+  # https://discourse.nixos.org/t/some-loose-ends-for-sway-on-nixos-which-we-should-fix/17728
+  # https://github.com/NixOS/nixpkgs/issues/57602
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+  };
 }

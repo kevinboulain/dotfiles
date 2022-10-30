dd576bfc265a1b36fa3585629ee2f801cc6162aa
Kevin Boulain <kevinboulain@gmail.com>
2022-10-30 11:11:55 +0100

    nixos: shuffle around the networking configuration

--- nixos/common/virtualisation.nix
+++ nixos/common/virtualisation.nix
@@ -5,6 +5,36 @@
     qemu
   ];
 
+  # A bridge without any physical interface serving as a gateway.
+  # TAP interfaces can be attached for NAT.
+  networking.bridges.nat0.interfaces = [];
+  networking.interfaces.nat0 = {
+    ipv4.addresses = [
+      {
+        address = "192.168.42.1";
+        prefixLength = 24;
+      }
+    ];
+  };
+  networking.nat = {
+    enable = true;
+    enableIPv6 = true;
+    internalInterfaces = [ "nat0" ];
+  };
+
+  # Set up dnsmasq to provide DHCP and DNS to clients attached to nat0.
+  networking.firewall.interfaces.nat0.allowedUDPPorts = [ 53 67 ];
+  services.dnsmasq = {
+    enable = true;
+    resolveLocalQueries = false;
+    extraConfig = ''
+      interface = nat0
+      bind-interfaces
+      dhcp-range = 192.168.42.100,192.168.42.200,1d
+      dhcp-authoritative
+    '';
+  };
+
   environment.etc."qemu/bridge.conf" = {
     mode = "0644";
     text = ''

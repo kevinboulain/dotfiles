79ec29648ebd104b896791bcc05151c49779e41a
Kevin Boulain <kevinboulain@gmail.com>
2023-02-20 22:14:25 +0100

    nixos: man pages

--- nixos/common/packages.nix
+++ nixos/common/packages.nix
@@ -3,7 +3,18 @@
   # Get rid of any package installed by default (like nano).
   environment.defaultPackages = [];
 
+  # https://discourse.nixos.org/t/missing-man-pages/4680/6
+  # Some, but not all, of these are true by default.
+  documentation = {
+    enable = true;
+    dev.enable = true;
+    man.enable = true;
+  };
+
   environment.systemPackages = with pkgs; [
+    man-pages
+    man-pages-posix
+
     file
     lsof
     strace

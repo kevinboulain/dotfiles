a6b0e4f9eeaeef52fa06c1b0fd35c1f7c8afaa68
Kevin Boulain <kevinboulain@gmail.com>
2023-03-07 23:56:24 +0100

    firefox: never store passwords

--- firefox/user.js
+++ firefox/user.js
@@ -5,3 +5,6 @@ user_pref("apz.gtk.kinetic_scroll.delta_mode", 2);
 
 // https://shivering-isles.com/til/2021/07/firefox-webrtc-popup
 user_pref("privacy.webrtc.legacyGlobalIndicator", false);
+
+// Never ask me to remember passwords.
+user_pref("signon.rememberSignons", false);

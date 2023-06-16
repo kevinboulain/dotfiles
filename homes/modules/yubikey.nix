d4f2d94dbe4c137f0f0aa935ae74a475d1ef2c9d
Kevin Boulain <kevin@boula.in>
2023-06-16 23:12:21 +0200

    yubikey: forgot important option

--- homes/modules/yubikey.nix
+++ homes/modules/yubikey.nix
@@ -1,9 +1,10 @@
 { pkgs, ... }: {
   # To set up OpenPGP: https://github.com/drduh/YubiKey-Guide
   # To set up OpenSSH:
-  #  ssh-keygen -t ed25519-sk -O resident -O application=ssh:git@github.com -f ~/.ssh/github
+  #  ssh-keygen -t ed25519-sk -O resident -O verify-required -O application=ssh:git@github.com -f ~/.ssh/github.com
   #  ssh-keygen -K
-  # Note the 'private' key isn't private, only a handle.
+  # Note the 'private' key isn't private, only a handle. And without
+  # 'verify-required', the PIN would not be asked.
   home.packages = with pkgs; [
     gnupg
     (pass.withExtensions (extensions: with extensions; [ pass-otp ]))

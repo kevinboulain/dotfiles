76f9115993c4dd67d68e14e17da04e6857c9d5cb
Kevin Boulain <kevin@boula.in>
2024-05-13 09:37:09 +0200

    bump

--- hosts/modules/packages/default.nix
+++ hosts/modules/packages/default.nix
@@ -2,6 +2,7 @@
 {
   # Get rid of any package installed by default (like nano).
   environment.defaultPackages = [ ];
+  programs.nano.enable = false; # Nano has been removed from environment.defaultPackages...
   # And of documentation.
   documentation.enable = lib.mkDefault false;
 }

30e5d432e9c3efb972347d5001ff1b3e001432c9
Kevin Boulain <kevin@boula.in>
2024-05-31 18:38:16 +0200

    tmux: remove obsolete stuff

--- homes/modules/tmux/default.nix
+++ homes/modules/tmux/default.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    # I'm not sure I want to opt into Home Manager's defaults...
-    tmux
-  ];
+  # I don't think I'll opt into Home Manager's tmux module: it's too opinionated
+  # and I would have to undo things (e.g.: the terminal is still set to screen
+  # though the terminfo database has long been updated), like for Bash...
+  home.packages = with pkgs; [ tmux ];
   xdg.configFile.tmux = {
     source = ./tmux;
     recursive = true;
